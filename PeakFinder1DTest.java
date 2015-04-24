a85dbfb5ae49eb36b19a18d6563d3c9e3d77e5e7
dennislixin <dennislixin@gmail.com>
2015-04-24 16:27:45 +0900

    converted peak finder to a static moethod instead of a class.

--- PeakFinder1DTest.java
+++ PeakFinder1DTest.java
@@ -13,34 +13,27 @@ public class PeakFinder1DTest
     for(int i=0; i<size; i++)
       array[i] = random.nextInt(size + 1);
 
-    PeakFinder finder = new PeakFinder<Integer>(array);
     System.out.println(Arrays.toString(array));
-    System.out.println("peak = " + finder.find());
+    System.out.println("peak = " + findPeak(array));
   }
-}
 
-class PeakFinder <E extends Comparable<? super E>>{
-  private E[] array;
-  public PeakFinder(E[] array){
+  public static <E extends Comparable<? super E>> E findPeak(E[] array){
     if(array == null || array.length == 0)
-      throw new IllegalArgumentException("array cannot be null or 0");
-    this.array = array;
+      return null;
+
+    return findPeakUtil(array, 0, array.length-1);
   }
 
-  public E find(){
-    return findUtil(0, array.length - 1);
-  }
+  private static <E extends Comparable<? super E>> E findPeakUtil(E[] array, int left, int right){
+    int mid = (right - left)/2 + left;
+    int newLeft = mid + 1;
+    int newRight = mid - 1;
 
-  private E findUtil(int left, int right){
-      int mid = (right - left)/2 + left;
-      int newLeft = mid + 1;
-      int newRight = mid - 1;
-
-      if(newLeft <= right && array[newLeft].compareTo(array[mid]) > 0)
-        return findUtil(newLeft, right);
-      else if(newRight >= left && array[newRight].compareTo(array[mid]) > 0)
-        return findUtil(left, newRight);
-      else
-        return array[mid];
+    if(newLeft <= right && array[newLeft].compareTo(array[mid]) > 0)
+      return findPeakUtil(array, newLeft, right);
+    else if(newRight >= left && array[newRight].compareTo(array[mid]) > 0)
+      return findPeakUtil(array, left, newRight);
+    else
+      return array[mid];
   }
 }
